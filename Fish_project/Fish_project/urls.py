c430939f6ea5b4ad59073c8c9e6b501bf140645c
Sergei Baryshevskii <black3knight@gmail.com>
2023-05-31 18:36:43 +0300

    Added redoc + updated requiremenets

--- Fish_project/Fish_project/urls.py
+++ Fish_project/Fish_project/urls.py
@@ -1,7 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# from django.conf.urls import url # need to figure with this re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +18,24 @@ if settings.DEBUG:
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
  
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Fish project API",
+      default_version='v1',
+      description="Документация для приложения quizzles проекта Fish-project",
+      # terms_of_service="URL страницы с пользовательским соглашением",
+      contact=openapi.Contact(email="black3knight@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+   re_path(r'^swagger(?P<format>\.json|\.yaml)$', 
+       schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
+       name='schema-swagger-ui'),
+   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+       name='schema-redoc'),
+] 
